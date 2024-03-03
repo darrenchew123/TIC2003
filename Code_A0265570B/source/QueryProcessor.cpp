66086e99d67572dd39ce79c409d51f1f593e7140
Ming Soon Tee <tee_ming_soon@hotmail.com>
2024-03-03 20:29:48 +0800

    query on all 12qns ok

--- Code_A0265570B/source/QueryProcessor.cpp
+++ Code_A0265570B/source/QueryProcessor.cpp
@@ -1,51 +1,98 @@
 #include "QueryProcessor.h"
+#include "Database.h"
+#include <iostream>
+#include <string>
+#include <unordered_set>
+
+using namespace std;
 
 QueryProcessor::QueryProcessor() {}
 
 QueryProcessor::~QueryProcessor() {}
 
-//Validate token type
-bool QueryProcessor::typeValidator(const string &token){
-    if(token == "procedure" || token == "variable" || token == "constant" || token == "assign" || token == "print" || token == "read" || token == "stmt")
-        return true;
-    return false;
+bool QueryProcessor::isT(const string &token) {
+    if (token == "*")
+        return 1;
+    return 0;
 }
 
-//Process query file to store declared objects that will be used fir validation by processSelect
-void QueryProcessor::processObjects(const vector<string>& tokens, unordered_map<string, string> &declaredObjects) {
-    //Process declared object eg. variable v;,constant c;
-    for (int i = 0; i < tokens.size(); i++) {
-        if (typeValidator(tokens[i])) {
-            if (i + 1 < tokens.size()) {
-                declaredObjects[tokens[i + 1]] = tokens[i];
-                i += 1;
-            }
+string QueryProcessor::checkQuotationMarks_returnArg(int &currIdx, const vector<string> &tokens) {
+    
+    string res = "";
+
+    if (tokens[currIdx] == "\"") {
+        
+        currIdx++;
+
+        while (currIdx < tokens.size() && tokens[currIdx] != "\"") {
+            res += tokens[currIdx];
+            currIdx++;
         }
+        currIdx++;
     }
+    else {
+        res = tokens[currIdx];
+        currIdx++;
+    }
+    return res;
 }
 
-//Process query file to store into selectObjects to be processed by statements
-void QueryProcessor::processSelect(const vector<string>& tokens, unordered_map<string, string> declaredObjects, vector<pair<string, string>>& selectObjects) {
+Query QueryProcessor::parser(const vector<string>& tokens) {
+    Query query;
+
+    unordered_set<string> conditionTypes{ "Follows", "Modifies", "Uses", "Parent" };
+    unordered_set<string> patternTypes{ "pattern" };
+
+
     for (int i = 0; i < tokens.size(); ++i) {
+
         if (tokens[i] == "Select") {
-            if (i + 1 < tokens.size()) {
-                string selectIdentifier = tokens[i + 1];
-                if (declaredObjects.find(selectIdentifier) != declaredObjects.end()) {
-                    selectObjects.push_back(make_pair(declaredObjects[selectIdentifier], selectIdentifier));
-                } else {
-                    throw runtime_error("Error: Identifier '" + selectIdentifier + "' not declared.");
-                }
+
+            query.selectType = tokens[i + 1]; // Assuming the next token is the select type
+            i++; // Move past the select type
+        }
+        else if (tokens[i] == "such" && tokens[i + 1] == "that") {
+            // Parse conditions like "Follows*", "Modifies", "Parent*", "Uses"
+            i += 2;
+            // cout << tokens[i+1];
+            //check if next token is "*"
+            bool flag_isT = isT(tokens[i + 1]);
+            // cout << flag_isT;
+            if (conditionTypes.count(tokens[i]) && flag_isT) {
+
+                query.condition.type = tokens[i];
+                query.condition.isT = true;
+
+                i += 3;
+                //check for quotation marks on left/right args
+                query.condition.leftArg = checkQuotationMarks_returnArg(i, tokens);
+
+                i++;
+                query.condition.rightArg = checkQuotationMarks_returnArg(i, tokens);
+
+            }
+            else if (conditionTypes.count(tokens[i])) {
+
+                query.condition.type = tokens[i];
+
+                i += 2;
+                query.condition.leftArg = checkQuotationMarks_returnArg(i, tokens);
+                i++;
+                query.condition.rightArg = checkQuotationMarks_returnArg(i, tokens);
             }
         }
+        //check for pattern combinations
+        else if (patternTypes.count(tokens[i])) {
+
+            query.pattern.var = tokens[i + 1];
+            i += 3;
+            query.pattern.patternLeftArg = checkQuotationMarks_returnArg(i, tokens);
+            i++;
+            query.pattern.patternRightArg = checkQuotationMarks_returnArg(i, tokens);
+        }
     }
-}
 
-//Start process to parse query
-void QueryProcessor::parser(const vector<string>& tokens, vector<pair<string, string>>& selectObjects) {
-    unordered_map<string, string> declaredObjects;
-
-    processObjects(tokens,declaredObjects);
-    processSelect(tokens,declaredObjects,selectObjects);
+    return query;
 }
 
 //Evalutate query to get result from DB
@@ -53,41 +100,45 @@ void QueryProcessor::evaluate(string query, vector<string>& output) {
 	output.clear();
 
     vector<string> databaseResults;
-    vector<pair<string, string>> selectObjects;
+
     Tokenizer tk;
 	vector<string> tokens;
 	tk.tokenize(query, tokens);
 
-    parser(tokens,selectObjects);
+    Query queryToExecute = parser(tokens);
 
+    output.push_back(queryToExecute.selectType);
+    output.push_back(queryToExecute.condition.type);
+    output.push_back(queryToExecute.condition.leftArg);
+    output.push_back(queryToExecute.condition.rightArg);
+    output.push_back(queryToExecute.pattern.var);
+    output.push_back(queryToExecute.pattern.patternLeftArg);
+    output.push_back(queryToExecute.pattern.patternRightArg);
 
-	for (int i = 0; i < selectObjects.size(); i++) {
-
-		string selectType = selectObjects[i].first;
-
-		if (selectType == "procedure") {
-			Database::getProcedures(databaseResults);
-		}
-        else if (selectType == "variable") {
-            Database::getVariables(databaseResults);
-        }
-        else if (selectType == "constant") {
-            Database::getConstants(databaseResults);
-        }
-		else if (selectType == "assign") {
-			Database::getStatementType(selectType,databaseResults);
-		}
-		else if (selectType == "print") {
-			Database::getStatementType(selectType,databaseResults);
-		}
-        else if (selectType == "read") {
-            Database::getStatementType(selectType,databaseResults);
-        }
-        else if (selectType == "stmt") {
-            Database::getStatements(databaseResults);
-        }
-	}
-	for (string databaseResult : databaseResults) {
-		output.push_back(databaseResult);
-	}
+    /*if (queryToExecute.selectType == "p") {
+        Database::getProcedures(databaseResults);
+    }
+    else if (queryToExecute.selectType == "v") {
+        Database::getProcedures(databaseResults);
+    }
+    else if (queryToExecute.selectType == "c") {
+        Database::getProcedures(databaseResults);
+    }
+    else if (queryToExecute.selectType == "a") {
+        Database::getProcedures(databaseResults);
+    }
+    else if (queryToExecute.selectType == "pr") {
+        Database::getProcedures(databaseResults);
+    }
+    else if (queryToExecute.selectType == "rd") {
+        Database::getProcedures(databaseResults);
+    }
+    else if (queryToExecute.selectType == "s") {
+        Database::getProcedures(databaseResults);
+    }
+    
+    for (string databaseresult : databaseResults) {
+        output.push_back(databaseresult);
+    }*/
 }
+
--- Code_A0265570B/source/QueryProcessor.h
+++ Code_A0265570B/source/QueryProcessor.h
@@ -8,19 +8,44 @@
 
 using namespace std;
 
+struct Query {
+
+    string selectType;
+
+    struct Condition {
+
+        string type;
+        string leftArg;
+        string rightArg;
+        bool isT = false;
+    } condition;
+
+    struct Pattern {
+
+        string var;
+        string patternLeftArg;
+        string patternRightArg;
+
+    } pattern;
+};
+
 class QueryProcessor {
 public:
 	QueryProcessor();
 
     ~QueryProcessor();
 
-    bool typeValidator(const string &token);
+    //bool typeValidator(const string &token);
 
-    void processObjects(const vector<string>& tokens, unordered_map<string, string> &declaredObjects);
+    //void processObjects(const vector<string>& tokens, unordered_map<string, string> &declaredObjects);
 
-    void processSelect(const vector<string>& tokens, unordered_map<string, string> declaredObjects, vector<pair<string, string>>& selectObjects);
+    //void processSelect(const vector<string>& tokens, unordered_map<string, string> declaredObjects, vector<pair<string, string>>& selectObjects);
 
-    void parser(const vector<string>& tokens, vector<pair<string, string>>& selectObjects);
+    Query parser(const vector<string>& tokens);
 
 	void evaluate(string query, vector<string>& results);
+
+    bool QueryProcessor::isT(const string &token);
+
+    string QueryProcessor::checkQuotationMarks_returnArg(int &currIdx, const vector<string> &tokens);
 };
